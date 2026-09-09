097fbeea93d00bad8f7b3b0b9eb9980684ad0934
Sarkar Azeem <azeemiqbal281@gmail.com>
2026-09-09 11:38:45 +0500

    Restore original regional route: Dubai wedding budget

---
+++ app/uae/dubai-wedding-budget-calculator/page.tsx
@@ -0,0 +1,3 @@
+'use client';
+import { WeddingCalculator } from '@/components/calculators/WeddingCalculator';
+export default function DubaiWeddingBudgetPage() { return <WeddingCalculator initialCountry="UAE" />; }
