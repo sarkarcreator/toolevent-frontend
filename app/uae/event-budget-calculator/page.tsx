07f037c9346d0be31fbb6163c32586b4228f913f
Sarkar Azeem <azeemiqbal281@gmail.com>
2026-09-09 11:39:03 +0500

    Restore original regional route: UAE event budget

---
+++ app/uae/event-budget-calculator/page.tsx
@@ -0,0 +1,3 @@
+'use client';
+import { BudgetCalculator } from '@/components/calculators/BudgetCalculator';
+export default function UAEEventBudgetPage() { return <BudgetCalculator initialCountry="UAE" />; }
