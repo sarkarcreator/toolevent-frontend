ec55c3f3c6c524c02c38feffe3a6980444e8138e
Sarkar Azeem <azeemiqbal281@gmail.com>
2026-09-09 11:38:35 +0500

    Fix original calculator route: event checklist

---
+++ app/tools/event-checklist-generator/page.tsx
@@ -0,0 +1,3 @@
+'use client';
+import { ChecklistGenerator } from '@/components/calculators/ChecklistGenerator';
+export default function EventChecklistGeneratorPage() { return <ChecklistGenerator />; }
